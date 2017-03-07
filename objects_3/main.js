168796c733bb595f23dd6d280c65495152dd4199
danwillscott <danwillscott@gmail.com>
2017-03-06 21:14:30 -0800

    made minor changes to objects 3 using prototype

--- objects_3/main.js
+++ objects_3/main.js
@@ -3,53 +3,56 @@
  */
 
 function VehicleConstructor(name, wheels, passengers, speed) {
-    //Private Vars
-    var distance_travelled = 0;
-    var vin = Math.floor((Math.random() * 100000000000) + 1)
     //Public Vars
+    this.distance_travelled = 0;
+    this.vin = Math.floor((Math.random() * 100000000000) + 1)
     this.name = name;
     this.wheels = wheels;
     this.passengers = passengers;
     this.speed = speed;
-
     this.sound = '';
-
-    // this.
-
-    function updateDistanceTravelled() {
-        distance_travelled += speed
-    }
-
-    this.move = function () {
-        updateDistanceTravelled();
-        this.make_noise();
-    };
-
-    this.make_noise = function () {
-        console.log(this.sound)
-    };
-
-    this.passenger_count = function () {
-        console.log(this.passengers)
-    };
-
-    this.check_miles = function () {
-        console.log(distance_travelled)
-    };
-
-    this.check_vin = function () {
-        console.log(vin)
-    };
 
 }
 
+VehicleConstructor.prototype.wheel_count = function () {
+    console.log(this.wheels);
+};
 
+VehicleConstructor.prototype.updateDistanceTravelled = function() {
+    this.distance_travelled += this.speed;
+};
+
+VehicleConstructor.prototype.move = function () {
+    this.updateDistanceTravelled();
+    this.make_noise();
+};
+
+VehicleConstructor.prototype.make_noise = function () {
+    console.log(this.sound);
+};
+
+VehicleConstructor.prototype.passenger_count = function () {
+    console.log(this.passengers);
+};
+
+VehicleConstructor.passenger_count = function () {
+    console.log(this.passangers);
+};
+
+VehicleConstructor.prototype.check_miles = function () {
+    console.log(this.distance_travelled);
+};
+
+VehicleConstructor.prototype.check_vin = function () {
+    console.log(this.vin);
+};
 
 
 // Bike instance of vehicle
 bike = new VehicleConstructor('Bike', 2, 1, 10);
 bike.sound = 'Ring Ring!';
 bike.make_noise();
+bike.wheel_count();
 console.log('***** end bike *****');
 
 // Car instance of vehicle
@@ -60,6 +63,7 @@ car.passenger_count();
 car.move();
 car.check_miles();
 car.check_vin();
+car.wheel_count();
 console.log('***** end car *****');
 // Bus instance of vehicle
 bus = new VehicleConstructor('Bus', 6, 1, 80);
@@ -70,7 +74,7 @@ bus.passengers_pickup = function (people) {
 };
 // Adding drop off function for buss
 bus.passengers_drop = function (people) {
-    bus.passengers -= people
+    bus.passengers -= people;
 };
 // Testing bus new functions
 bus.passengers_pickup(100);
@@ -84,6 +88,7 @@ bus.move();
 bus.move();
 bus.check_miles();
 bus.check_vin();
+bus.wheel_count();
 console.log('***** end bus *****');
 
 
